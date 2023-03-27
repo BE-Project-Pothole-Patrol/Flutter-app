4d40aaebd54f806ecc36515b9aeb695139c7f485
Tanay Kamath <2tanaykamath@gmail.com>
2023-03-27 20:07:24 +0530

    add light theme and launcher icon

--- lib/theme/theme_constants.dart
+++ lib/theme/theme_constants.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const kPrimaryColor = Color(0xFF2377FC);
+const kPrimaryColorDark = Color(0xFF0A244C);
+const kBackgroundColor = Color.fromARGB(255, 255, 255, 255);
 
 ThemeData lightTheme = ThemeData(
-  brightness: Brightness.light
+  scaffoldBackgroundColor: kBackgroundColor,
+  primaryColor: kPrimaryColor,
+  primaryColorDark: kPrimaryColorDark,
+  appBarTheme: const AppBarTheme(
+    backgroundColor: kPrimaryColor, //use your hex code here
+  ),
+  fontFamily: 'Poppins'
 );
 
-ThemeData darkTheme = ThemeData(
-  brightness: Brightness.dark
-);
+ThemeData darkTheme = ThemeData(brightness: Brightness.dark);
