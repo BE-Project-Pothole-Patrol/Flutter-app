438e9f9a1b91075846693f114191ce21b0e2b63f
Tanay Kamath <2tanaykamath@gmail.com>
2023-03-27 22:07:02 +0530

    create app Splash Screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/logo.jpeg',
+                width: size.width * 0.7,
+                height: size.width * 0.7,
+              ),
+              SizedBox(
+                height: size.height * 0.05,
+              ),
+              const Text(
+                "PotNoHole",
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 30, 
+                  fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
